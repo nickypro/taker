37d996153a33808ceacdab4424fe84ed3fa98e06
Nicky Pochinkov <work@nicky.pro>
2024-02-02 13:56:20 +0000

    Added mistral ModelMap

--- src/taker/model_maps.py
+++ src/taker/model_maps.py
@@ -20,6 +20,7 @@ class ConfigClass:
     normalization_type: str
     architecture: str
     tokenizer_name: str
+    n_key_value_heads: int = None
     is_low_precision: bool = False
     use_attn_scale: bool = None
     use_local_attn: bool = None
@@ -48,75 +49,7 @@ def convert_hf_model_config(official_model_name: str):
     hf_config = AutoConfig.from_pretrained(official_model_name)
     architecture = hf_config.architectures[0]
 
-    if 'llama-7b' in official_model_name:
-        cfg_dict = {
-            "d_model": 4096,
-            "d_head": 4096 // 32,
-            "n_heads": 32,
-            "d_mlp": 11008,
-            "n_layers": 32,
-            "n_ctx": 2048,
-            "eps": 1e-6,
-            "d_vocab": 32000,
-            "act_fn": "silu",
-            "normalization_type": "RMS",
-            "positional_embedding_type": "rotary",
-            "rotary_dim": 4096 // 32,
-            "final_rms": True,
-            "gated_mlp": True,
-        }
-    elif 'llama-13b' in official_model_name:
-        cfg_dict = {
-            "d_model": 5120,
-            "d_head": 5120 // 40,
-            "n_heads": 40,
-            "d_mlp": 13824,
-            "n_layers": 40,
-            "n_ctx": 2048,
-            "eps": 1e-6,
-            "d_vocab": 32000,
-            "act_fn": "silu",
-            "normalization_type": "RMS",
-            "positional_embedding_type": "rotary",
-            "rotary_dim": 5120 // 40,
-            "final_rms": True,
-            "gated_mlp": True,
-        }
-    elif 'llama-30b' in official_model_name:
-        cfg_dict = {
-            "d_model": 6656,
-            "d_head": 6656 // 52,
-            "n_heads": 52,
-            "d_mlp": 17920,
-            "n_layers": 60,
-            "n_ctx": 2048,
-            "eps": 1e-6,
-            "d_vocab": 32000,
-            "act_fn": "silu",
-            "normalization_type": "RMS",
-            "positional_embedding_type": "rotary",
-            "rotary_dim": 6656 // 52,
-            "final_rms": True,
-            "gated_mlp": True,
-        }
-    elif 'llama-65b' in official_model_name:
-        cfg_dict = {
-            "d_model": 8192,
-            "d_head": 8192 // 64,
-            "n_heads": 64,
-            "d_mlp": 22016,
-            "n_layers": 80,
-            "n_ctx": 2048,
-            "eps": 1e-6,
-            "d_vocab": 32000,
-            "act_fn": "silu",
-            "normalization_type": "RMS",
-            "positional_embedding_type": "rotary",
-            "rotary_dim": 8192 // 64,
-            "final_rms": True,
-            "gated_mlp": True,
-        }
-    elif architecture.lower() == "LlamaForCausalLM".lower():
+    if architecture.lower() == "LlamaForCausalLM".lower():
         cfg_dict = {
             "d_model": hf_config.hidden_size,
             "d_head": hf_config.hidden_size // hf_config.num_attention_heads,
@@ -133,6 +66,26 @@ def convert_hf_model_config(official_model_name: str):
             "final_rms": True,
             "gated_mlp": True,
         }
+    elif architecture == "MistralForCausalLM":
+        cfg_dict = {
+            "d_model": hf_config.hidden_size,
+            "d_head": hf_config.hidden_size // hf_config.num_attention_heads,
+            "n_heads": hf_config.num_attention_heads,
+            "d_mlp": hf_config.intermediate_size,
+            "n_layers": hf_config.num_hidden_layers,
+            "n_ctx": hf_config.max_position_embeddings,
+            "eps": hf_config.rms_norm_eps,
+            "d_vocab": hf_config.vocab_size,
+            "act_fn": hf_config.hidden_act,
+            "normalization_type": "RMS",
+            "positional_embedding_type": "rotary",
+            "eps": hf_config.rms_norm_eps,
+            "n_key_value_heads": hf_config.num_key_value_heads,
+            "rotary_dim": hf_config.hidden_size // hf_config.num_attention_heads, #?
+            "use_local_attn": True,
+            "gated_mlp": True,
+        }
+
     elif architecture == "GPTNeoForCausalLM":
         cfg_dict = {
             "d_model": hf_config.hidden_size,
@@ -204,6 +157,7 @@ def convert_hf_model_config(official_model_name: str):
         }
     elif architecture == "GPTNeoXForCausalLM":
         cfg_dict = {
+            "model_type": "causal",
             "d_model": hf_config.hidden_size,
             "d_head": hf_config.hidden_size // hf_config.num_attention_heads,
             "n_heads": hf_config.num_attention_heads,
@@ -503,6 +457,106 @@ def build_llama_layer_map(cfg: ConfigClass):
         "mlp.b_out"     : lambda layer, _inpt=None: llama_mlp_bias(layer, "mlp.gate_proj", _inpt),
     }
     return llama_layer_map
+
+# Mistral Model
+###############
+
+mistral_model_map = {
+    "model"           : "model",
+    "layers"          : "model.layers",
+    "embed"           : "model.embed_tokens",
+    "embed.W_E"       : "model.embed.weights",
+    "pos_embed"       : "model.embed_positions",
+    "pos_embed.W"     : "model.embed_positions.weight",
+    "ln_final"        : "model.norm",
+    "ln_final.w"      : "model.norm.weight",
+    "unembed.W_U"     : "model.lm_head.weight.T",
+    "unembed.b_U"     : None,
+}
+
+def build_mistral_layer_map(cfg: ConfigClass):
+    attn_proj_map = {"q": "q_proj", "k": "k_proj", "v": "v_proj", "o": "o_proj"}
+    mlp_proj_map = {"mlp.in_proj": "up_proj", "mlp.out_proj": "down_proj", "mlp.gate_proj": "gate_proj"}
+
+    def mistral_qkv_weight(layer, key: str, inpt: Optional[Any]=None):
+        # Prepare shape changing
+        if key in ['k', 'v']:  # Adjust for Mistral's unique n_key_value_heads
+            their_shape = "(n_key_value_heads d_head) d_model"
+        else:
+            their_shape = "(n_heads d_head) d_model"
+        my_shape = "n_heads d_head d_model"
+        sizes = generate_sizes_dict(my_shape, cfg)
+
+        # Get attn proj module
+        attn = layer.self_attn
+        attn_proj = get_attrs(attn, attn_proj_map[key])
+
+        # Get mode
+        if inpt is None:
+            W = attn_proj.weight
+            W = einops.rearrange(W, f"{their_shape} -> {my_shape}", **sizes)
+            return W
+
+        # Set mode
+        W = einops.rearrange(inpt, f"{my_shape} -> {their_shape}", **sizes)
+        update_param(attn_proj, "weight", W)
+
+    def mistral_attn_bias(layer, key: str, _inpt: Optional[Any]=None):
+        # Create fake bias with zeros because is easier to handle
+        their_shape = "(n_heads d_head)"
+        my_shape = "n_heads d_head"
+        sizes = generate_sizes_dict(my_shape, cfg)
+
+        attn = layer.self_attn
+        _proj = get_attrs(attn, attn_proj_map[key]).weight
+        b = torch.zeros(_proj.shape[:-1], dtype=_proj.dtype, device=_proj.device)
+        if key == "o":
+            return b
+        return einops.rearrange(b, f"{their_shape} -> {my_shape}", **sizes)
+
+    def mistral_mlp_bias(layer, key: str, _inpt: Optional[Any]=None):
+        mlp = layer.mlp
+        _proj = get_attrs(mlp, mlp_proj_map[key]).weight
+        b = torch.zeros(_proj.shape[:-1], dtype=_proj.dtype, device=_proj.device)
+        return b
+
+    mistral_layer_map = {
+        "ln1"           : "input_layernorm",
+        "ln1.w"         : "input_layernorm.weight",
+        "ln1.b"         : None,
+
+        "attn"          : "self_attn",
+        "attn.q_proj"   : "self_attn.q_proj",
+        "attn.k_proj"   : "self_attn.k_proj",
+        "attn.v_proj"   : "self_attn.v_proj",
+
+        **generate_attn_qkv_functions(mistral_qkv_weight, mistral_attn_bias),
+
+        "attn.inv_out_proj" : "self_attn.inv_out_proj",
+        "attn.out_proj" : "self_attn.o_proj",
+        "attn.W_O"      : "self_attn.o_proj.weight",
+        "attn.b_O"      : lambda layer, _inpt=None: mistral_attn_bias(layer, "o", _inpt),
+
+        "ln2"           : "post_attention_layernorm",
+        "ln2.w"         : "post_attention_layernorm.weight",
+        "ln2.b"         : None,
+
+        "mlp.in_proj"   : "mlp.up_proj",
+        "mlp.gate_proj" : "mlp.gate_proj",
+        "mlp.W_in"      : "mlp.up_proj.weight",
+        "mlp.W_gate"    : "mlp.gate_proj.weight",
+        "mlp.b_in"      : lambda layer, _inpt=None: mistral_mlp_bias(layer, "mlp.in_proj", _inpt),
+        "mlp.b_gate"    : lambda layer, _inpt=None: mistral_mlp_bias(layer, "mlp.out_proj", _inpt),
+
+        "activation_fn" : "mlp.act_fn",
+
+        "mlp.out_proj"  : "mlp.down_proj",
+        "mlp.W_out"     : "mlp.down_proj.weight",
+        "mlp.b_out"     : lambda layer, _inpt=None: mistral_mlp_bias(layer, "mlp.gate_proj", _inpt),
+
+    }
+
+    return mistral_layer_map
 
 # GPT NEO X and Pythia Models
 #############################
@@ -938,6 +992,8 @@ def get_model_key_map(config: ConfigClass):
         return opt_model_map
     if architecture in ["LLaMAForCausalLM", "LlamaForCausalLM"]:
         return llama_model_map
+    if architecture == "MistralForCausalLM":
+        return mistral_model_map
     if architecture == "GPTNeoXForCausalLM":
         return gpt_neox_model_map
     if architecture == "GPT2LMHeadModel":
@@ -956,6 +1012,8 @@ def get_layer_key_map(config: ConfigClass):
         return build_opt_layer_map(config)
     if architecture in ["LLaMAForCausalLM", "LlamaForCausalLM"]:
         return build_llama_layer_map(config)
+    if architecture == "MistralForCausalLM":
+        return build_mistral_layer_map(config)
     if architecture == "GPTNeoXForCausalLM":
         return build_gpt_neox_layer_map(config)
     if architecture == "GPT2LMHeadModel":
