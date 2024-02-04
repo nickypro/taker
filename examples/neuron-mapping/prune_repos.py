78cea1965b1db0ca0622a4a38a0d642fc114ce05
TetraspaceW <holomanga@gmail.com>
2024-02-04 11:59:51 +0000

    move get_shared_pruning_data to its own function to support vision transformer pruning

--- examples/neuron-mapping/prune_repos.py
+++ examples/neuron-mapping/prune_repos.py
@@ -29,45 +29,59 @@ def compare_pruned_ff_criteria(cripple_repos: list[str], model_size: str):
             ratio = torch.sum(matches)/torch.sum(repo1_ff_criteria)
             ratios[repo1][repo2] = ratio
             
-    return ratios
+    return ratios 
     
+def get_shared_pruning_data(
+        model_repo: str = "nickypro/tinyllama-15M",
+        cripple_repos: list[str] = ["physics", "biology","chemistry", "math", "code"],
+        focus_repo: str = "pile"
+    ):
+    
+    # Configure initial model and tests
+    c = PruningConfig(
+        wandb_project = "testing", # repo to push results to
+        model_repo   = model_repo,
+        # "metallama/llama-2-7b"
+        token_limit  = 1000,  # trim the input to this max length
+        run_pre_test = True,  # evaluate the unpruned model
+        eval_sample_size = 1e3,
+        collection_sample_size = 1e3,
+        # Removals parameters
+        ff_frac   = 0.2,     # % of feed forward neurons to prune
+        attn_frac = 0.00,     # % of attention neurons to prune
+        focus     = focus_repo, # the “reference” dataset
+        cripple   = "physics",          # the “unlearned” dataset
+        additional_datasets=tuple(), # any extra datasets to evaluate on
+        recalculate_activations = False, # iterative vs non-iterative
+        n_steps = 1,
+    )
 
-# Configure initial model and tests
-c = PruningConfig(
-    wandb_project = "testing", # repo to push results to
-    model_repo   = "nickypro/tinyllama-15M",
-    # "metallama/llama-2-7b"
-    token_limit  = 1000,  # trim the input to this max length
-    run_pre_test = True,  # evaluate the unpruned model
-    eval_sample_size = 1e3,
-    collection_sample_size = 1e3,
-    # Removals parameters
-    ff_frac   = 0.2,     # % of feed forward neurons to prune
-    attn_frac = 0.00,     # % of attention neurons to prune
-    focus     = "pile", # the “reference” dataset
-    cripple   = "physics",          # the “unlearned” dataset
-    additional_datasets=tuple(), # any extra datasets to evaluate on
-    recalculate_activations = False, # iterative vs non-iterative
-    n_steps = 1,
-)
+    # Parse CLI for arguments
+    # c, args = cli_parser(c)
 
-# Parse CLI for arguments
-# c, args = cli_parser(c)
+    #list of repos to cripple
+    ff_frac_to_prune = [0.01,0.02,0.05,0.1,0.2]
+    model_size = c.model_repo.split('-')[-1]
 
-#list of repos to cripple
-cripple_repos = ["physics", "biology","chemistry", "math", "code"]
-ff_frac_to_prune = [0.01,0.02,0.05,0.1,0.2]
-model_size = c.model_repo.split('-')[-1]
+    # Run the iterated pruning for each cripple repo, for a range of ff_frac pruned
+    shared_pruning_data = {}
+    for ff_frac in ff_frac_to_prune:
+        c.ff_frac = ff_frac
+        for repo in cripple_repos:
+            c.cripple = repo
+            print("running iteration for ", c.cripple, " vs ", c.focus, "with ff_frac: ", ff_frac)
+            with torch.no_grad():
+                model, history = run_pruning(c)
+        ratios = compare_pruned_ff_criteria(cripple_repos, model_size)
+        shared_pruning_data[ff_frac] = ratios
+    
+    return shared_pruning_data
 
-# Run the iterated pruning for each cripple repo, for a range of ff_frac pruned
-shared_pruning_data = {}
-for ff_frac in ff_frac_to_prune:
-    c.ff_frac = ff_frac
-    for repo in cripple_repos:
-        c.cripple = repo
-        print("running iteration for ", c.cripple, " vs ", c.focus, "with ff_frac: ", ff_frac)
-        with torch.no_grad():
-            model, history = run_pruning(c)
-    ratios = compare_pruned_ff_criteria(cripple_repos, model_size)
-    shared_pruning_data[ff_frac] = ratios
+shared_pruning_data = get_shared_pruning_data(
+    model_repo="Ahmed9275/Vit-Cifar100",
+    cripple_repos=["cifar20-veh1", "cifar20-veh2"],
+    focus_repo= "cifar20-split")
 print(shared_pruning_data)
+
+# shared_pruning_data = get_shared_pruning_data("nickypro/tinyllama-15M")
+# print(shared_pruning_data)
