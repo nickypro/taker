70b99bd15675149de217a0c104e4f6c95adc7a47
Rashid <rashidur92@hotmail.com>
2024-02-16 00:58:53 +0000

    moved functions around, cleaned up old unused code

--- examples/neuron-mapping/compare.py
+++ examples/neuron-mapping/compare.py
@@ -1,5 +1,7 @@
 import torch
 from taker.activations import get_top_frac
+import matplotlib.pyplot as plt
+import numpy as np
 
 #takes repo name, model size and timestamp as strings and returns ff_scores tensor by loading relevant file
 def load_tensors_for_repo(repo, model_size="hf", timestamp="recent"):
@@ -49,6 +51,41 @@ def save_pruning_data_dict( model_size: str,
     torch.save( data, filename )
     print( f'Saved {filename} to {model_size}' )
     return filename
+
+def plot_ratios(ratios):
+    datasets = cripple_repos
+    grid = [
+        [
+            ratios[0.01][dataset_a][dataset_b].item() if dataset_a != dataset_b else np.nan
+            for dataset_b in datasets
+        ]
+        for dataset_a in datasets
+    ]
+    grid = np.ma.masked_where(np.isnan(grid), grid)
+
+    average = np.mean(grid)
+
+    plt.imshow(grid)
+    for i in range(len(datasets)):
+        for j in range(len(datasets)):
+            plt.text(
+                j,
+                i,
+                f"{grid[i, j]:.2f}",
+                ha="center",
+                va="center",
+                color="black" if grid[i, j] > average else "white",
+            )
+    plt.xticks(
+        range(len(datasets)), [dataset for dataset in datasets], rotation=90
+    )
+    plt.yticks(range(len(datasets)), [dataset for dataset in datasets])
+
+    plt.subplots_adjust(bottom=0.3)
+
+    plt.title(f"FF Criteria Overlap for Prune Ratio 0.01")
+
+    plt.show()
 
 cripple_repos = ["biology", "chemistry", "physics", "math", "code", "poems", "civil", "emotion", "pile_FreeLaw", "pile_PubMed_Abstracts", "pile_PubMed_Central", "pile_NIH_ExPorter", "pile_Enron_Emails", "pile_Github", "pile_StackExchange", "pile_HackerNews", "pile_ArXiv", "pile_Wikipedia", "pile_Ubuntu_IRC", "pile_USPTO_Backgrounds", "pile_PhilPapers", "pile_EuroParl", "pile_Gutenberg", "pile_PhilPapers", "pile_EuroParl", "pile_Gutenberg"]
 ff_fracs = [0.01, 0.02, 0.05, 0.1, 0.2, 0.5]
