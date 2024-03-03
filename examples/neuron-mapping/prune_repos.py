aa53b711e0c378b3c7371e78c2eb3f914f753603
TetraspaceW <holomanga@gmail.com>
2024-03-03 22:27:59 +0000

    Tensors from random text model

--- examples/neuron-mapping/prune_repos.py
+++ examples/neuron-mapping/prune_repos.py
@@ -35,7 +35,7 @@ c = PruningConfig(
     wandb_project = "testing", # repo to push results to
     # model_repo   = "nickypro/tinyllama-15M",
     # model_repo   = "facebook/opt-1.3b",
-    model_repo   = "NousResearch/Llama-2-7b-hf",
+    model_repo   = "nickypro/llama-7b-hf-rand",
     token_limit  = 1000,  # trim the input to this max length
     run_pre_test = False,  # evaluate the unpruned model
     eval_sample_size = 1e5,
@@ -47,12 +47,13 @@ c = PruningConfig(
     cripple   = "physics",          # the “unlearned” dataset
     additional_datasets=tuple(), # any extra datasets to evaluate on
     recalculate_activations = False, # iterative vs non-iterative
-    dtype = "int8",
+    dtype = "int4",
     n_steps = 1,
 )
 
 #list of repos to cripple
-cripple_repos = ["emotion", "pile_FreeLaw", "pile_PubMed_Abstracts", "pile_PubMed_Central", "pile_NIH_ExPorter", "pile_Enron_Emails", "pile_Github", "pile_StackExchange", "pile_HackerNews", "pile_ArXiv", "pile_Wikipedia", "pile_Ubuntu_IRC", "pile_USPTO_Backgrounds", "pile_PhilPapers", "pile_EuroParl", "pile_Gutenberg", "pile_PhilPapers", "pile_EuroParl", "pile_Gutenberg"]
+#cripple_repos = ["emotion", "pile_FreeLaw", "pile_PubMed_Abstracts", "pile_PubMed_Central", "pile_NIH_ExPorter", "pile_Enron_Emails", "pile_Github", "pile_StackExchange", "pile_HackerNews", "pile_ArXiv", "pile_Wikipedia", "pile_Ubuntu_IRC", "pile_USPTO_Backgrounds", "pile_PhilPapers", "pile_EuroParl", "pile_Gutenberg", "pile_PhilPapers", "pile_EuroParl", "pile_Gutenberg", 'code', 'poems', 'civil', 'chemistry']
+cripple_repos = ['physics', 'biology', 'math']
 
 #prune each repo and save tensors, doing some extra computation but only really need ff_scores for each repo, will do actual pruning for different values of ff_frac in compare.py
 for repo in cripple_repos:
