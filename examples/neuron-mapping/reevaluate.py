03ecb34db158680374c2c564437ab1418d1b00b7
Rashid <rashidur92@hotmail.com>
2024-03-10 19:01:14 +0000

    fixed error with find accuracy function

--- examples/neuron-mapping/reevaluate.py
+++ examples/neuron-mapping/reevaluate.py
@@ -27,8 +27,8 @@ c = PruningConfig(
     model_repo   = "NousResearch/Llama-2-7b-hf",
     token_limit  = 1000,  # trim the input to this max length
     run_pre_test = False,  # evaluate the unpruned model
-    eval_sample_size = 1e4,
-    collection_sample_size = 1e4,
+    eval_sample_size = 1e3,
+    collection_sample_size = 1e3,
     # Removals parameters
     ff_frac   = 0.01,     # % of feed forward neurons to prune
     attn_frac = 0.00,     # % of attention neurons to prune
@@ -66,7 +66,8 @@ all_datasets = ["biology",
 
 test_datasets = ["biology", "chemistry"]
 
-def find_accuracy(dataset, ff_frac):
+#pruning dataset is the dataset to use to determine which neurons to prune, target dataset is the dataset to find the accuracy of
+def find_accuracy(pruning_dataset, target_dataset, ff_frac):
     opt = Model(
         c.model_size,
         limit=c.token_limit,
@@ -77,7 +78,7 @@ def find_accuracy(dataset, ff_frac):
         mask_fn=c.mask_fn,
         )
     
-    eval_config: EvalConfig = infer_dataset_config(dataset)
+    eval_config: EvalConfig = infer_dataset_config(target_dataset)
     eval_config.num_tokens_to_skip = c.collection_sample_size
     eval_config.sample_size = c.eval_sample_size
     print(f"ff_frac is: {ff_frac}")
@@ -85,21 +86,21 @@ def find_accuracy(dataset, ff_frac):
         base_accuracy = run_evaluation(opt, eval_config)
         print(f"base accuracy is: {base_accuracy}")
         return base_accuracy.percent["base"]
-    ff_criteria = get_ff_criteria_for_ff_frac(dataset, ff_frac)
+    ff_criteria = get_ff_criteria_for_ff_frac(pruning_dataset, ff_frac)
     opt.delete_ff_keys(ff_criteria)
     eval_data = run_evaluation(opt, eval_config)
     print(f"accuracy for ff_frac {ff_frac} is {eval_data.percent}")
     return eval_data.percent["base"]
 
+
 #binary search to find ff_frac for a given target accuracy, upto given precision while being below target
 #also check if should compare base accuracy or topk accuracy.
 def find_correct_ff_frac(dataset: str, target_accuracy: float, accuracy_precision: float, lower=0, upper=1):
     print(f"trying to find correct ff_frac for {dataset} with target accuracy {target_accuracy}")
-    
 
     while upper >= lower:
         print(f"binary search with lower {lower} and upper {upper}")
-        acc_mid = find_accuracy(dataset, (lower + upper)/2)
+        acc_mid = find_accuracy(dataset, dataset, (lower + upper)/2)
         if acc_mid >= target_accuracy-accuracy_precision and acc_mid <= target_accuracy:
             return (lower + upper)/2
         elif acc_mid < target_accuracy:
@@ -113,13 +114,15 @@ def compareEvaluations(datasets):
     final_data = {}
     for dataset1 in datasets:
         print("for neurons pruned based on: ", dataset1)
-        unpruned_accuracy = find_accuracy(dataset1, 0)
-        ff_frac = find_correct_ff_frac(dataset1, target_accuracy=(0.8*unpruned_accuracy), accuracy_precision=5, upper=0.2)
+        unpruned_accuracy = find_accuracy(dataset1, dataset1, 0)
+        target_accuracy=0.8*unpruned_accuracy
+        ff_frac = find_correct_ff_frac(dataset1, target_accuracy=target_accuracy, accuracy_precision=5, upper=0.256)
         final_data[dataset1] = {}
-        final_data[dataset1]['unpruned_accuracy'] = unpruned_accuracy
+        final_data[dataset1]["unpruned_accuracy"] = unpruned_accuracy
+        final_data[dataset1]["target_accuracy"] = target_accuracy
         for dataset2 in datasets:
             print("finding accuracy for: ", dataset2)
-            final_data[dataset1][dataset2] = find_accuracy(dataset2, ff_frac)
+            final_data[dataset1][dataset2] = find_accuracy(dataset1, dataset2, ff_frac)
     return final_data
 
 answer = compareEvaluations(test_datasets)
