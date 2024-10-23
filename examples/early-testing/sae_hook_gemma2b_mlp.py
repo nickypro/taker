0c350777eb4d878f867e24e03e1d0b3ad78ef51b
Nicky Pochinkov <work@nicky.pro>
2024-10-23 16:43:18 +0000

    made outputs more readable

--- examples/early-testing/sae_hook_gemma2b_mlp.py
+++ examples/early-testing/sae_hook_gemma2b_mlp.py
@@ -3,16 +3,19 @@
 # note: 26x layers with 16k width, d_in=2306, d_sae=16384
 # total parameters: 2 * 26 * 2306 * 16384 = 1_964_638_208
 
+import torch
+from tqdm import tqdm
 from taker import Model
+torch.set_grad_enabled(False)
 
 hook_config = """
 post_mlp: sae_encode, collect, sae_decode
 """
-m = Model("google/gemma-2-2b", hook_config=hook_config)
+m = Model("google/gemma-2-2b", limit=1000, hook_config=hook_config)
 
 # evaluate the model
 from taker.eval import evaluate_all
-evaluate_all(m, 1e3, ["pile"])
+evaluate_all(m, 1e4, ["pile"])
 
 # show that the collection hooks are working, and that the SAE is not loaded
 m.hooks.enable_collect_hooks(["post_mlp"])
@@ -23,7 +26,7 @@ for layer in range(m.cfg.n_layers):
 m.hooks.disable_all_collect_hooks()
 
 # load the SAE
-for layer in range(m.cfg.n_layers):
+for layer in tqdm(range(m.cfg.n_layers), desc="Loading SAE"):
     sae_hook = m.hooks.neuron_sae_encode[f"layer_{layer}_post_mlp"]
     # >>> SAE.from_pretrained("gemma-scope-2b-pt-mlp-canonical", "layer_0/width_16k/canonical")
     sae_hook.load_sae_from_pretrained("gemma-scope-2b-pt-mlp-canonical", f"layer_{layer}/width_16k/canonical")
@@ -37,4 +40,4 @@ for layer in range(m.cfg.n_layers):
 m.hooks.disable_all_collect_hooks()
 
 # evaluate the model again
-evaluate_all(m, 1e3, ["pile"])
+evaluate_all(m, 1e4, ["pile"])
